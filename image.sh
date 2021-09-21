73bab4673f45006fe53af64c6a1a68c704b54ae9
idideads <idideads@gmail.com>
2021-09-21 13:01:06 +0800

    run

--- image.sh
+++ image.sh
@@ -10,17 +10,23 @@ images=(
     kube-proxy:v1.22.2
     pause:3.5
     etcd:3.5.0-0
-    coredns/coredns:v1.8.4
+    # coredns/coredns:v1.8.4
 )
 
 for imageName in ${images[@]} ; do
     docker pull ${gcr_registry}/$imageName
-    if [ $imageName = "coredns/coredns:v1.8.4" ]
-    then
-        docker tag ${gcr_registry}/$imageName ${ali_registry}/${ali_namespace}/coredns:v1.8.4
-        docker push ${ali_registry}/${ali_namespace}/coredns:v1.8.4
-    else
-        docker tag ${gcr_registry}/$imageName ${ali_registry}/${ali_namespace}/${imageName}
-        docker push ${ali_registry}/${ali_namespace}/${imageName}
-    fi
+    # if [ $imageName = "coredns/coredns:v1.8.4" ]
+    # then
+    #     docker tag ${gcr_registry}/$imageName ${ali_registry}/${ali_namespace}/coredns:v1.8.4
+    #     docker push ${ali_registry}/${ali_namespace}/coredns:v1.8.4
+    # else
+    #     docker tag ${gcr_registry}/$imageName ${ali_registry}/${ali_namespace}/${imageName}
+    #     docker push ${ali_registry}/${ali_namespace}/${imageName}
+    # fi
+    docker tag ${gcr_registry}/$imageName ${ali_registry}/${ali_namespace}/${imageName}
+    docker push ${ali_registry}/${ali_namespace}/${imageName}
 done
+
+docker pull ${gcr_registry}/coredns/coredns:v1.8.4
+docker tag ${gcr_registry}/coredns/coredns:v1.8.4 ${ali_registry}/${ali_namespace}/coredns:v1.8.4
+docker push ${ali_registry}/${ali_namespace}/coredns:v1.8.4
